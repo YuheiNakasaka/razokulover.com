cdf882008dd949cc2c935984f88b074980abf6b2
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-05-02 13:50:38 +0900

    ssrしないようにする

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
-  <section class="container">
-    <div>
-      <div class="icon">
-        <img src="~/assets/razokulover-icon.png"/>
+  <no-ssr>
+    <section class="container">
+      <div>
+        <div class="icon">
+          <img src="~/assets/razokulover-icon.png"/>
+        </div>
+        <h1 class="title">
+          razokulover
+        </h1>
+        <nav>
+          <ul class="links">
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="https://twitter.com/razokulover" target="_blank">Twitter</a></li>
+            <li><a href="https://www.facebook.com/YuheiNakasaka" target="_blank">Facebook</a></li>
+            <li><a href="https://razokulover.hateblo.jp/" target="_blank">Blog</a></li>
+            <li><a href="https://github.com/YuheiNakasaka" target="_blank">GitHub</a></li>
+          </ul>
+        </nav>
       </div>
-      <h1 class="title">
-        razokulover
-      </h1>
-      <nav>
-        <ul class="links">
-          <li><a href="/profile">Profile</a></li>
-          <li><a href="https://twitter.com/razokulover" target="_blank">Twitter</a></li>
-          <li><a href="https://www.facebook.com/YuheiNakasaka" target="_blank">Facebook</a></li>
-          <li><a href="https://razokulover.hateblo.jp/" target="_blank">Blog</a></li>
-          <li><a href="https://github.com/YuheiNakasaka" target="_blank">GitHub</a></li>
-        </ul>
-      </nav>
-    </div>
-  </section>
+    </section>
+  </no-ssr>
 </template>
 
 <script>
