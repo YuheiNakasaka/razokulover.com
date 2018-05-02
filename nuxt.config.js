175a6e6afcb4a743afd68e6a4ed78ab89162a021
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-05-02 10:49:02 +0900

    PWAの設定追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'razokulovercom',
+    title: 'razokulover',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -34,5 +34,8 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  modules: [
+    '@nuxtjs/pwa',
+  ]
 }
