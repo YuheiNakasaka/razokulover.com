b6a5ddda0904bc1eaa8c68fd77344a60bcada9ae
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-02-28 16:08:34 +0900

    add ag_viewer about page

---
+++ pages/ag_viewer/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div class="section">
+      <div class="group">
+        <h1>このアプリについて</h1>
+        <p>このアプリは<a href="https://www.agqr.jp/">超!A&amp;G+</a>とは全く関係のない非公式のアプリです。現在超!A&amp;G+のiOSアプリは長年更新されておらず、視聴しづらい環境となっています。このアプリはそうした公式のアプリで視聴が困難となっている方などにむけて有志で作成されています。非公式のアプリなので広告は設置しておりませんのでご安心ください。</p>
+      </div>
+      <div class="group">
+        <h1>機能</h1>
+        <p>・ライブ配信を視聴できる</p>
+        <p>・#agqrのハッシュタグがついたツイートを閲覧できる</p>
+        <p>・番組表を閲覧できる</p>
+        <p>・お気に入りに設定した番組の放送時間が近づくと通知を受け取れる</p>
+      </div>
+      <div class="group">
+        <h1>その他</h1>
+        <p>・非公式のアプリなので超!A&amp;G+公式の都合により配信できなくなることがあります。</p>
+        <p>・超!A&amp;G+の利益を毀損させることは意図しておりません。申し立てには即応じます。</p>
+        <p>・必要であればアプリのソースコードごと提供することも可能ですのでご連絡いただければ幸いです。</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.container {
+  min-height: 100vh;
+  padding: 0 30px 30px 30px;
+  .section {
+    margin: 0 0 30px 0;
+    .group {
+      margin: 30px 0 0 0;
+    }
+  }
+}
+</style>
